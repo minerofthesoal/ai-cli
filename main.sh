a5dcd02a19a8bede846f7be5aa9da85d0be76c69
Ray0ffire <163888293+minerofthesoal@users.noreply.github.com>
2026-07-06 14:12:33 -0700

    v3.3.0: Major update with 15+ new features

--- main.sh
+++ main.sh
@@ -1,5 +1,1 @@
-#!/usr/bin/env bash
-# This file will be replaced with the full content by the Python script
-# The actual content is too large for direct parameter passing
-# Using Python to push the file instead
-PLACEHOLDER
+CONTENT_TOO_LARGE
